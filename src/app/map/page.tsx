a4d50e1f0870e73094618e56ad6c27b004c7c24e
Рябко Ілія Олегович <ilyaryabko22@gmail.com>
2025-08-17 14:47:44 +0000

    useSearchParams() should be wrapped in a suspense boundary at page "/map

--- src/app/map/page.tsx
+++ src/app/map/page.tsx
@@ -1,12 +1,21 @@
 import Header from '@/components/Header';
 import MapView from '@/components/MapView';
+import { Skeleton } from '@/components/ui/skeleton';
+import { Suspense } from 'react';
 
 export default function MapPage() {
   return (
     <div className="flex flex-col min-h-screen">
       <Header />
       <main className="flex-1">
-        <MapView />
+        <Suspense fallback={
+          <div className="relative w-full h-[calc(100vh-57px)] flex items-center justify-center">
+              <Skeleton className="w-full h-full" />
+              <p className="absolute text-lg text-muted-foreground">Loading Map...</p>
+          </div>
+        }>
+          <MapView />
+        </Suspense>
       </main>
     </div>
   );

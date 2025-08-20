7b9f3e8ac174a6b1d32c6f16325a6a2bd99e4bb9
Рябко Ілія Олегович <ilyaryabko22@gmail.com>
2025-08-20 21:36:48 +0000

    lets recreate sign in an sign up options. We dont need to ask user for n

--- src/app/payment/page.tsx
+++ src/app/payment/page.tsx
@@ -1,3 +1,4 @@
+
 import Header from '@/components/Header';
 import { Card, CardHeader, CardTitle, CardDescription, CardContent, CardFooter } from '@/components/ui/card';
 import { Button } from '@/components/ui/button';
